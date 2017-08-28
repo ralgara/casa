b1f8818dbd6a27b59459d7c790b90ee9cc1a7f39
Rafael Algara <rafael.algara@clarabridge.com>
2017-08-28 09:37:48 -0400

    Add init.el

--- .live-packs/ralgara-pack/init.el
+++ .live-packs/ralgara-pack/init.el
@@ -1,1 +1,18 @@
-/home/ralgara/.live-packs/ralgara-pack/init.el
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+;; Saved-desktop heaven
+(desktop-save-mode)
+;; Fast window switching: g'bye C-x,o...
+(windmove-default-keybindings)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(setq shell-command-switch "-ic") ;; ensure shells load ~/.bashrc (-i)
+
+;; C-c left           restore window configuration (e.g. after C-x 1), from winner-mode
+;; M-;                comment block
