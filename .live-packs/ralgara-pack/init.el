c6cc8cc2c9665e1739a368830c6b3598afc7c7a3
Rafael Algara <ralgara@gmail.com>
2018-09-20 16:54:23 -0400

    Emacs: larger default font

--- .live-packs/ralgara-pack/init.el
+++ .live-packs/ralgara-pack/init.el
@@ -5,11 +5,11 @@
 (desktop-change-dir "~/.emacs.d")
 (desktop-save-mode 1)
 
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-18"))
+
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
 (require 'neotree)
-
 (global-set-key [f8] 'neotree-toggle)
 
 ;; Fast window switching: g'bye C-x,o...
